6da7fb302b630924282fcd6ab005c77951e16dda
ABHATANK <abhatank14@gmail.com>
2023-04-15 22:09:40 +0530

    First Commit

---
+++ src/main/java/com/ABHA/FolderLocker/AFL.java
@@ -0,0 +1,11 @@
+package com.ABHA.FolderLocker;
+
+import com.ABHA.FolderLocker.ui.MainUI;
+ class AFL {
+
+   
+    public static void main(String[] args) {
+        new MainUI().setApp();       
+    }
+    
+}
---
+++ src/main/java/com/ABHA/FolderLocker/ui/AppConstants.java
@@ -0,0 +1,8 @@
+
+package com.ABHA.FolderLocker.ui;
+
+
+public class AppConstants {
+    public static final String APP_DIR = "/FolderLocker/";
+    public static final String APP_LOG_PATH = "/FolderLocker/log/";
+}
